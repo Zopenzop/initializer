8986d1666bfe34b06474b2cf306273142147ad8e
Zopenzop <77222182+Zopenzop@users.noreply.github.com>
2022-01-30 16:49:28 +0530

    Update event_handler.js

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -6,7 +6,7 @@ module.exports = (client, Discord) => {
     const event_files = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
     for(const file of event_files){
       count++;
-      const event = require(`../../events/${dirs}/${file}`);
+      const event = require(path.resolve(`./events/${dirs}/${file}`));
       const event_name = file.split('.')[0];
       //client.events.set(event_name, event);
       //client.on(event_name, event.bind(null, Discord, client, message, reaction, user))
