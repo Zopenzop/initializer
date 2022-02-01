4c61581020c55a1a38a555143b164f8a8ab15f8f
Zopenzop <77222182+Zopenzop@users.noreply.github.com>
2022-02-01 10:16:53 +0530

    Update initializer.js

--- initializer.js
+++ initializer.js
@@ -40,7 +40,7 @@ module.exports = async (module_name, token, mongo_path) => {
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS],
 	  partials: ['MESSAGE', , 'GUILD_MEMBER', 'CHANNEL', 'REACTION', 'USER'],
   });
-/*const {
+const {
 	registerFont
 } = require('canvas');
 registerFont('./fonts/Roboto-Regular.ttf', {
@@ -51,7 +51,7 @@ registerFont('./fonts/Roboto-Medium.ttf', {
 });
 registerFont('./fonts/Roboto-Bold.ttf', {
 	family: 'Roboto-Bold'
-});*/
+});
   client.functions = require('./functions.js');
   const { loading } = require('./functions.js')
   const mongo = require('./mongo.js')
